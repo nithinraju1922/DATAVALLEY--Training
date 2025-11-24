59e8f4405107fcafecdb7bbb43938259bd1b55c6
nithinraju1922 <nithinrajuchilaka@gmail.com>
2025-11-24 21:44:08 +0530

    upadte of operators

--- src/programs/operators.java
+++ src/programs/operators.java
@@ -1,9 +1,25 @@
 package programs;
-
+import java.util.*;
 public class operators {
 	public static void main(String[] args) {
-		int a=10;
-		System.out.println();
+		Scanner sc=new Scanner (System.in);
+		System.out.println("Operators in java:");
+		System.out.println("Arithemetic Operators");
+		System.out.println("Bit wise Operators");
+		System.out.println("Conditional operators");
+		System.out.println("Logical operators");
+		System.out.println("Decrement or Increment operators");
+		System.out.println("Combinational operators");
+		System.out.println("Terninary operators");
+		System.out.println("BITWISE OPERATOR:");
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println((a&b));
+		System.out.println("OR:"+(a|b));
+		System.out.println("XOR:"+(a^b));
+		System.out.println("Not:"+(~a));
+		System.out.println("left shift:"+(a>>b));
+		System.out.println("right shift:"+(a<<b));
 	}
 	
 
